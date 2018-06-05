4c37ab518224473df2520b8d60e1856c5f9a98a6
Salman Fakhri <salmanfakhri2@gmail.com>
2018-06-05 13:16:45 -0400

    attempting to add mobile

---
+++ Mobile/VideoSyncApp/VideoSyncApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoSyncApp
+//
+//  Created by Salman Fakhri on 6/5/18.
+//  Copyright © 2018 Salman Fakhri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
